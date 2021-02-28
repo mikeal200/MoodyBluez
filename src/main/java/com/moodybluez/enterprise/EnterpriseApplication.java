e401ce0e65f7b619ef345ace330e694b023588f5
Michael Williams <58800020+mikeal200@users.noreply.github.com>
2021-02-28 15:37:57 -0500

    bootstrap navbars formatting is off

--- src/main/java/com/moodybluez/enterprise/EnterpriseApplication.java
+++ src/main/java/com/moodybluez/enterprise/EnterpriseApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EnterpriseApplication {
+public class 	EnterpriseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EnterpriseApplication.class, args);
