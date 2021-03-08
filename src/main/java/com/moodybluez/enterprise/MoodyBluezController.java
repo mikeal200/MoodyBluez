83722fe3db61e44eef97bcd61887d8250df28cf2
Stephen Meckstroth <meckstss@gmail.com>
2021-03-07 19:38:18 -0500

    Implemented json api for Mood using id as a path variable

--- src/main/java/com/moodybluez/enterprise/MoodyBluezController.java
+++ src/main/java/com/moodybluez/enterprise/MoodyBluezController.java
@@ -1,17 +1,31 @@
 package com.moodybluez.enterprise;
 
+import com.moodybluez.enterprise.dao.MoodDAO;
+import com.moodybluez.enterprise.dto.Mood;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MoodyBluezController {
 
     /**
      * Handles the root endpoint and returns index.html
-     * @return
+     * @return Returns a thymeleaf template
      */
     @RequestMapping("/")
     public String index() {
         return "index";
     }
+
+    /**
+     * Returns the json representation of a Mood by id
+     * @param id integer id of a mood
+     * @return Mood in JSON format
+     */
+    @GetMapping("/api/Mood/{id}")
+    public @ResponseBody Mood moodFetchById(@PathVariable int id){
+        MoodDAO dao = new MoodDAO();
+        return dao.fetchByMoodID(id);
+    }
+
 }
