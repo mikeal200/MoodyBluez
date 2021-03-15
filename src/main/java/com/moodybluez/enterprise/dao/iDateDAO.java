47febe94d0b3afe533852942558586c4971acd11
manchava <manchava@mail.uc.edu>
2021-03-15 15:18:25 -0400

    Update iDateDAO.java

--- src/main/java/com/moodybluez/enterprise/dao/iDateDAO.java
+++ src/main/java/com/moodybluez/enterprise/dao/iDateDAO.java
@@ -1,5 +1,12 @@
 package com.moodybluez.enterprise.iDateDAO;
 
 interface iDateDAO {
+    Date save(Date date) throws Exception;
+    List<Date> fetchAll();
 
+    Date fetch(string date);
+
+    void delete(string date);
+
+    List<Date> fetchDatesByDayOfWeekId(int dayofweekId)
 }
