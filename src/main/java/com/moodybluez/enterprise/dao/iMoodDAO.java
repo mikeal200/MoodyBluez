61a4d8bcc3a0b41758ec9926813e5bffa6d16916
manchava <manchava@mail.uc.edu>
2021-03-15 15:38:00 -0400

    Update iMoodDAO.java

--- src/main/java/com/moodybluez/enterprise/dao/iMoodDAO.java
+++ src/main/java/com/moodybluez/enterprise/dao/iMoodDAO.java
@@ -1,5 +1,14 @@
 package com.moodybluez.enterprise.iMoodDAO;
 
 interface iMoodDAO {
-    
+    Mood save(Mood mood) throws Exception;
+
+    List<Mood> fetchAll();
+
+    Mood fetch(int MoodId);
+
+    void delete(int MoodId);
+
+    List<Mood> fetchMoodsByMoodId(int moodId);
+
 }
