c622ee485bbf67734a32cab038a7c913b9334998
Michael Williams <58800020+mikeal200@users.noreply.github.com>
2021-03-18 16:31:04 -0400

    added application-test.properties

--- src/main/java/com/moodybluez/enterprise/dao/UserSQLDAO.java
+++ src/main/java/com/moodybluez/enterprise/dao/UserSQLDAO.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@Profile("dev")
+@Profile({"dev", "default"})
 public class UserSQLDAO implements IUserDAO {
 
     @Autowired
