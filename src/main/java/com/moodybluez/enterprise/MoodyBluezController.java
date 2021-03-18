369a898f4de1866d032a2271f0a0fa4c5c52c9d8
Michael Williams <58800020+mikeal200@users.noreply.github.com>
2021-03-18 16:52:52 -0400

    added postmapping(/register)

--- src/main/java/com/moodybluez/enterprise/MoodyBluezController.java
+++ src/main/java/com/moodybluez/enterprise/MoodyBluezController.java
@@ -1,17 +1,41 @@
 package com.moodybluez.enterprise;
 
+import com.moodybluez.enterprise.dto.User;
+import com.moodybluez.enterprise.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MoodyBluezController {
 
+    @Autowired
+    IUserService userService;
+
     /**
      * Handles the root endpoint and returns index.html
-     * @return
+     * @return returns index page
      */
     @RequestMapping("/")
     public String index() {
+        return "index";
+    }
+
+    /**
+     * Handles the register endpoint where users register
+     * with a unique username and password
+     * @param user user object that will be saved into database
+     * @return returns index page
+     */
+    @PostMapping("/register")
+    public String saveUser(User user) {
+        try {
+            userService.save(user);
+        } catch(Exception e) {
+            e.printStackTrace();
+            return "index";
+        }
         return "index";
     }
 }
