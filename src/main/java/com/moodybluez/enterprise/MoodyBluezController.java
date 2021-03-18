759d4ee38794ef8cd52d1a68b08aec88162b3215
Michael Williams <58800020+mikeal200@users.noreply.github.com>
2021-03-18 19:41:30 -0400

    registration complete, user input into database with secure password

--- src/main/java/com/moodybluez/enterprise/MoodyBluezController.java
+++ src/main/java/com/moodybluez/enterprise/MoodyBluezController.java
@@ -4,8 +4,12 @@ import com.moodybluez.enterprise.dto.User;
 import com.moodybluez.enterprise.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 @Controller
 public class MoodyBluezController {
@@ -18,7 +22,7 @@ public class MoodyBluezController {
      * @return returns index page
      */
     @RequestMapping("/")
-    public String index() {
+    public String index(User user) {
         return "index";
     }
 
@@ -28,7 +32,7 @@ public class MoodyBluezController {
      * @param user user object that will be saved into database
      * @return returns index page
      */
-    @PostMapping("/register")
+    /*@RequestMapping("/register")
     public String saveUser(User user) {
         try {
             userService.save(user);
@@ -36,6 +40,25 @@ public class MoodyBluezController {
             e.printStackTrace();
             return "index";
         }
+        return "index";
+    }*/
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+
+        return "index";
+    }
+
+
+    @PostMapping("/process_register")
+    public String processRegister(User user) throws Exception {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
+        userService.save(user);
+
         return "index";
     }
 }
