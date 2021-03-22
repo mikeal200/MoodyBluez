75ab62f19144ad1e4ee293ff455267aeb5a670cd
Michael Williams <58800020+mikeal200@users.noreply.github.com>
2021-03-22 12:56:07 -0400

    test to fix tests on circleci

--- src/main/java/com/moodybluez/enterprise/EnterpriseApplication.java
+++ src/main/java/com/moodybluez/enterprise/EnterpriseApplication.java
@@ -2,9 +2,6 @@ package com.moodybluez.enterprise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
 @SpringBootApplication
 public class EnterpriseApplication {
@@ -13,9 +10,4 @@ public class EnterpriseApplication {
 		SpringApplication.run(EnterpriseApplication.class, args);
 	}
 
-	@Bean(name="entityManagerFactory")
-	public LocalSessionFactoryBean sessionFactory() {
-		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-		return sessionFactory;
-	}
 }
