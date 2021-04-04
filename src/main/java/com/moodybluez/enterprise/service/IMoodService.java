5a1ab0ea4d20fe0b8676ab16b3e004fee20a282c
allieprusha <prushaar@mail.uc.edu>
2021-04-04 12:06:15 -0400

    Use one blank line in javadoc before first @param

--- src/main/java/com/moodybluez/enterprise/service/IMoodService.java
+++ src/main/java/com/moodybluez/enterprise/service/IMoodService.java
@@ -5,6 +5,7 @@ import com.moodybluez.enterprise.dto.Mood;
 public interface IMoodService {
     /**
      * fetch a mood with a given ID
+     *
      * @param id unique identifier for a mood
      * @return the matching mood, or null if no matches found
      */
