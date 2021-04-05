8407ac503ee1ef512db50d9c5e9744121dbe6489
Michael Williams <58800020+mikeal200@users.noreply.github.com>
2021-04-05 19:38:48 -0400

    clean up imports

--- src/main/java/com/moodybluez/enterprise/EnterpriseApplication.java
+++ src/main/java/com/moodybluez/enterprise/EnterpriseApplication.java
@@ -2,10 +2,8 @@ package com.moodybluez.enterprise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
 @EnableCaching
