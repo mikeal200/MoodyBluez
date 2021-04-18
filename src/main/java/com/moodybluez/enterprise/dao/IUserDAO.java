2f3db6a2108fbe9a241ffff8aa43a22fc973b51d
manchava <manchava@mail.uc.edu>
2021-04-18 18:19:24 -0400

    Adding JavaDoc to IUserDAO

--- src/main/java/com/moodybluez/enterprise/dao/IUserDAO.java
+++ src/main/java/com/moodybluez/enterprise/dao/IUserDAO.java
@@ -4,7 +4,15 @@ import com.moodybluez.enterprise.dto.Mood;
 import com.moodybluez.enterprise.dto.User;
 
 import java.util.Map;
+/**
+ * @author
+ */
 
+/**
+ * allows user to enter details about themself
+ *
+ * @param username
+ */
 public interface IUserDAO {
     User save(User user);
 
