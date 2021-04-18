2342bb8bb0ef0dd68c3ad2d5cf5b05feaa217df6
manchava <manchava@mail.uc.edu>
2021-04-18 18:13:35 -0400

    Adding JavaDoc to IMoodDAO

--- src/main/java/com/moodybluez/enterprise/dao/IMoodDAO.java
+++ src/main/java/com/moodybluez/enterprise/dao/IMoodDAO.java
@@ -3,7 +3,16 @@ package com.moodybluez.enterprise.dao;
 import com.moodybluez.enterprise.dto.Mood;
 
 import java.util.Map;
+/**
+ * @author
+ */
 
+/**
+ * Allows the user to enter information about their mood
+ *
+ * @param moodId a number that helps identify each mood
+ *
+ */
 public interface IMoodDAO {
 
     Mood save(Mood mood);
