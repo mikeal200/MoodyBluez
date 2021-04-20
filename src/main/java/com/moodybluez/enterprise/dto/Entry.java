03c19898e43d691b1084430fe5cac7aa834b5491
manchava <manchava@mail.uc.edu>
2021-04-19 22:52:53 -0400

    Creating JavaDoc in Entry.java

--- src/main/java/com/moodybluez/enterprise/dto/Entry.java
+++ src/main/java/com/moodybluez/enterprise/dto/Entry.java
@@ -8,6 +8,16 @@ import java.util.Objects;
 
 @Entity
 public @Data
+
+/**
+ * getters and setters
+ *
+ * @param entryId the entryId is a unique identifier for each entry
+ * @param userId the userId is a unique identifier for each user
+ * @param moodId the moodId is a unique identifier for each mood
+ * @param date the date is the date of the entry
+ * @param description the description is the description provided by the user
+ */
 class Entry {
     private int entryId;
     private int userId;
@@ -18,12 +28,23 @@ class Entry {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "entryId", nullable = false)
+
+/**
+ *
+ * Gets the unique identifier for the entry.
+ * @return entryId
+ */
     public int getEntryId() {
         return entryId;
     }
 
     @Basic
     @Column(name = "userId", nullable = false)
+
+/**
+ * Gets the unique identifier for the user.
+ * @return userId
+ */
     public int getUserId() {
         return userId;
     }
@@ -34,33 +55,60 @@ class Entry {
 
     @Basic
     @Column(name = "moodId", nullable = false)
+
+/**
+ * Gets the unique identifier for the mood.
+ * @return moodId
+ */
     public int getMoodId() {
         return moodId;
     }
 
+/**
+ * Sets the unique identifier for the mood.
+ *
+ */
     public void setMoodId(int moodId) {
         this.moodId = moodId;
     }
 
     @Basic
     @Column(name = "date", nullable = false, unique = false)
+
+/**
+ * Gets the date of the entry.
+ * @return date
+ */
     public Date getDate() {
         return date;
     }
 
+/**
+ * Sets the unique identifier for the date.
+ *
+ */
     public void setDate(Date date) {
         this.date = date;
     }
 
     @Basic
     @Column(name = "description", nullable = true, length = 500)
+/**
+ * Gets the description of the entry.
+ * @return description
+ */
     public String getDescription() {
         return description;
     }
 
+/**
+ * Sets the unique identifier for the description.
+ *
+ */
     public void setDescription(String description) {
         this.description = description;
     }
+
 
     @Override
     public boolean equals(Object o) {
