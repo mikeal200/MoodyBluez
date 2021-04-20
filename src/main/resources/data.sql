502f404abe5b865bdf85a06039f6bcd799736176
Michael Williams <58800020+mikeal200@users.noreply.github.com>
2021-04-20 15:23:16 -0400

    fixed moods being added repeatedly into database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO mood (description) VALUES
-('Sad'),
-('Anger'),
-('Joy'),
-('Disgust'),
-('Contempt'),
-('Surprise');
+INSERT IGNORE INTO mood (mood_id, description) VALUES
+(1, 'Sad'),
+(2, 'Anger'),
+(3, 'Joy'),
+(4, 'Disgust'),
+(5, 'Contempt'),
+(6, 'Surprise');
